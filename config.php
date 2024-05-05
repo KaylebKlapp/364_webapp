f7279e4f6c9b9bad82a034556375da73324498e5
lsobrien1096 <c24logan.obrien@afacademy.af.edu>
2024-05-05 14:26:28 -0600

    Create config.php

---
+++ config.php
@@ -0,0 +1,15 @@
+<?php
+
+// Configure credentials of the database
+$dbHost = 'localhost';
+$dbPort = '5432';
+$dbName = 'space-a';
+$dbUsername = 'student';
+$dbPassword = 'CompSci364';
+
+// Connect to Database
+$db = pg_connect("host=$dbHost port = $dbPort dbname=$dbName user=$dbUsername password=$dbPassword");
+if (!$db){
+    die("Error: Unable to connect to $dbName database. Please try again.");
+}
+?>
